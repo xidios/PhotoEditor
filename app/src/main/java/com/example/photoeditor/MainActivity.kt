9f3192e1936f2a6af210023e65a84349980ca666
Irina Plotnikova <ziangoru@gmail.com>
2021-04-22 00:38:07 +0700

    init

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.photoeditor
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+//private const val FILE_NAME = "photo.jpg"
+private const val REQUEST_CODE = 42
+private lateinit var photoFile: File
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
