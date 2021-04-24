fb4df021859694e2c016eb26b0265da9b4379959
xidios <47945350+xidios@users.noreply.github.com>
2021-04-24 17:58:28 +0700

    ничего полезного

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -1,41 +1,30 @@
 package com.example.photoeditor
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.util.*
 
-private const val FILE_NAME = "photo.jpg"
-private const val REQUEST_CODE = 42
-private lateinit var photoFile: File
+private lateinit var currentPhotoPath: String
+private val REQUEST_CAMERA = 1
+private val REQUEST_GALLERY = 2
 
 class MainActivity : AppCompatActivity() {
-    private fun saveImage(finalBitmap: Bitmap) {
-        val root = Environment.getExternalStorageDirectory().toString()
-        val myDir = File("$root/saved_images")
-        myDir.mkdirs()
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val fname = "Shutta_$timeStamp.jpg"
-        val file = File(myDir, fname)
-        if (file.exists()) file.delete()
-        try {
-            val out = FileOutputStream(file)
-            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-            out.flush()
-            out.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,17 +44,17 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnTakePicture.setOnClickListener(){
-            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(i, 101)
+            takeANewPhoto()
         }
 
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode==101){
-            var pic = data?.getParcelableExtra<Bitmap>("data")
+            val pic = BitmapFactory.decodeFile(currentPhotoPath)
             imageView.setImageBitmap(pic)
-            saveImage(pic!!)
+            var name = "${System.currentTimeMillis()}"
 
         }
     }
@@ -78,6 +67,31 @@ class MainActivity : AppCompatActivity() {
         if(requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
 
             btnTakePicture.isEnabled = true
+        }
+    }
+    private fun createImageFile(): File {
+        val timeStamp = java.text.SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "IMG_$timeStamp"
+
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(imageFileName, ".jpg", storageDir)
+
+        currentPhotoPath = image.absolutePath
+
+        return image
+    }
+
+
+    private fun takeANewPhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try {
+            val pathToImage = FileProvider.getUriForFile(this, "com.example.android.fileprovider", createImageFile())
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, pathToImage)
+            startActivityForResult(intent, REQUEST_CAMERA)
+        }
+
+        catch (e: IOException) {
         }
     }
 }
