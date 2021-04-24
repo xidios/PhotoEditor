ba5890e09ce09dfa09666110677b5d7bde2fc5b9
Irina Plotnikova <ziangoru@gmail.com>
2021-04-24 18:00:03 +0700

    Фото/Галерея

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -1,28 +1,135 @@
 package com.example.photoeditor
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.os.Parcelable
 import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 import java.io.File
-
-//private const val FILE_NAME = "photo.jpg"
-private const val REQUEST_CODE = 42
-private lateinit var photoFile: File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val IMAGE = "Image"
+    private val REQUEST_CAMERA = 1
+    private val REQUEST_GALLERY = 2
+
+    private lateinit var currentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnCamera.setOnClickListener {
+            if (checkPermissions()) {
+                takeANewPhoto()
+            }
+            else {
+                requestPermissions()
+            }
+        }
+        btnGallery.setOnClickListener {
+            if (checkPermissions()) {
+                pickFromGallery()
+            }
+            else {
+                requestPermissions()
+            }
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        var writeAccess = false
+        var readAccess = false
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED) {
+            readAccess = true
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED) {
+            writeAccess = true
+        }
+
+        return (readAccess && writeAccess)
+    }
+
+
+    private fun requestPermissions() {
+        val permissions = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+
+        ActivityCompat.requestPermissions(this, permissions, 0)
+    }
+
+
+    private fun createImageFile(): File {
+        // Classic title of the file with timestamp
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "IMG_$timeStamp"
+
+        // Picking a directory to save and creating the file
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(imageFileName, ".jpg", storageDir)
+
+        // Saving a path to the file
+        currentPhotoPath = image.absolutePath
+
+        return image
+    }
+
+    private fun takeANewPhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try {
+            val pathToImage = FileProvider.getUriForFile(this, "com.example.android.fileprovider", createImageFile())
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, pathToImage)
+            startActivityForResult(intent, REQUEST_CAMERA)
+        }
+
+        catch (e: IOException) {
+            toast("Error...")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
+                val uri = Uri.parse(currentPhotoPath)
+
+                imageView.setImageURI(uri)
+                startActivity(intent)
+            }
+
+            else if (requestCode == REQUEST_GALLERY) {
+                val uri = data!!.data
+
+                imageView.setImageURI(uri)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_GALLERY)
     }
 }
