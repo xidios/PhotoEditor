97d0f007451a109e06cdc4840ef469ebbb2ad552
Chantreck <vladnetaev@mail.ru>
2021-04-25 11:12:50 +0700

    rotate layout

--- app/src/main/java/com/example/photoeditor/RotateActivity.kt
+++ app/src/main/java/com/example/photoeditor/RotateActivity.kt
@@ -3,7 +3,7 @@ package com.example.photoeditor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Rotate : AppCompatActivity() {
+class RotateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rotate)
