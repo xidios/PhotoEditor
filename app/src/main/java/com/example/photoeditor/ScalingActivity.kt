9ed3499d78434a178ba9e262b0b08b77a78c26f6
xidios <47945350+xidios@users.noreply.github.com>
2021-05-04 01:27:53 +0700

    Переделал масштабирование
    После глобальных изменений изменил масштабирование, чтобы оно работало. Также сделал кнопку обрабатывающую сохранение изменений на изображении

--- app/src/main/java/com/example/photoeditor/ScalingActivity.kt
+++ app/src/main/java/com/example/photoeditor/ScalingActivity.kt
@@ -1,32 +1,47 @@
 package com.example.photoeditor
 
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.util.Log
 import android.widget.SeekBar
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_rotation.*
 import kotlinx.android.synthetic.main.activity_scaling.*
 import kotlinx.android.synthetic.main.fragment_save.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class ScalingActivity : AppCompatActivity() {
-
+    private val RESULT_TAG = "resultImage"
     private lateinit var NewPhoto: Bitmap
+    private lateinit var PhotoOnSave: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scaling)
 
-        var k: Int
-
-        val image = intent.getParcelableExtra<Parcelable>("Image")
-        imageViewScaling.setImageURI(image as Uri)
-        var photoBitmap: Bitmap = (imageViewScaling.drawable as BitmapDrawable).bitmap
-        NewPhoto = photoBitmap
+        val receivedImage = intent.getByteArrayExtra("IMAGE")
+        if (receivedImage != null) {
+            NewPhoto = BitmapFactory.decodeByteArray(receivedImage, 0, receivedImage.size)
+            PhotoOnSave = NewPhoto
+            imageViewScaling.setImageBitmap(NewPhoto)
+        }
+        buttonScalingApply.setOnClickListener(){
+            val resultIntent = Intent()
+            try {
+                resultIntent.putExtra(RESULT_TAG, compressBitmap(PhotoOnSave))
+                setResult(RESULT_OK, resultIntent)
+                Toast.makeText(this, "Изображение сохранено", Toast.LENGTH_SHORT).show()
+            } catch (error: Exception) {
+                Log.d("ScalingActivity", "Произошла ошибка при сохранении изображения"+error.message)
+            }
+        }
 
 
         seekBarScaling.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -34,7 +49,9 @@ class ScalingActivity : AppCompatActivity() {
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 val temp = 100 - i
                 imageViewScaling.setImageDrawable(null)
-                imageViewScaling.setImageBitmap(Scale(temp))
+                PhotoOnSave = Scale(temp)
+                imageViewScaling.setImageBitmap(PhotoOnSave)
+
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {
@@ -46,28 +63,6 @@ class ScalingActivity : AppCompatActivity() {
 
             }
         })
-
-//        buttonScaling.setOnClickListener() {
-//            k = seekBarScaling.progress
-//            val toast = Toast.makeText(
-//                applicationContext,
-//                k.toString(),
-//                Toast.LENGTH_SHORT
-//            )
-//            if (k > 0) {
-//                toast.show()
-//                imageViewScaling.setImageDrawable(null)
-//                imageViewScaling.setImageBitmap(zoom(k))
-//            } else {
-//                imageViewScaling.setImageDrawable(null)
-//                imageViewScaling.setImageBitmap(NewPhoto)
-//                Toast.makeText(
-//                    applicationContext,
-//                    "SHOUTERLGRE BE MORE THSN 0",
-//                    Toast.LENGTH_SHORT
-//                )
-//            }
-//        }
     }
 
 
