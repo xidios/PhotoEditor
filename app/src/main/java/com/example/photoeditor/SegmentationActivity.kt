9b74de5d1004084fdc6eeb1d010fb53a6c3c266e
xidios <47945350+xidios@users.noreply.github.com>
2021-05-07 21:50:44 +0700

    Подключил OpenCV
    Госпаде спасибо. Подключил библиотеку для дальнейшей сегментации лиц.

--- app/src/main/java/com/example/photoeditor/SegmentationActivity.kt
+++ app/src/main/java/com/example/photoeditor/SegmentationActivity.kt
@@ -2,10 +2,18 @@ package com.example.photoeditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.opencv.android.OpenCVLoader
 
 class SegmentationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segmentation)
+        if (OpenCVLoader.initDebug()) {
+            Log.d("myTag", "OpenCV loaded")}
+        else{
+            Log.d("myTag", "OpenCV not loaded")
+        }
     }
 }
