6a9ef26779902babd3ed1a22bb1295e43420541e
Chantreck <minetvtc@yandex.ru>
2021-05-12 21:41:57 +0700

    Revert "Merge branch 'Rotation' into BranchesUnite"
    This reverts commit 74553de0fc572bfbbee610b27c56da276e26cb9f, reversing
    changes made to 9c57da56d32fae0c8b3721a632203f1f00b6236f.

--- app/src/main/java/com/example/photoeditor/RotationActivity.kt
+++ app/src/main/java/com/example/photoeditor/RotationActivity.kt
@@ -1,44 +1,27 @@
 package com.example.photoeditor
 
-import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
-import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
+import android.net.Uri
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.example.photoeditor.algorithms.RotationImage
 import kotlinx.android.synthetic.main.activity_rotation.*
-import kotlinx.android.synthetic.main.fragment_save.*
+import org.jetbrains.anko.toast
 
 class RotationActivity : AppCompatActivity() {
-    private val RESULT_TAG = "resultImage"
-    private var image: Bitmap? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rotation)
 
         rotationToolbar.setNavigationOnClickListener{
-            this.finish()
+            finish()
         }
+        rotationImage.setImageURI(intent.getParcelableExtra<Parcelable>("Image") as Uri)
 
-        try {
-            val receivedImage = intent.getByteArrayExtra("IMAGE")
-            if (receivedImage != null) {
-                image = BitmapFactory.decodeByteArray(receivedImage, 0, receivedImage.size)
-                rotationImage.setImageBitmap(image)
-            }
-        } catch (e: Exception) {
-            Log.d("RotationActivity", e.toString())
-        }
-
-        applyRotationButton.setOnClickListener {
-            rotate()
-        }
+        applyRotationButton.setOnClickListener {rotate()}
     }
 
     private fun rotate() {
@@ -48,24 +31,19 @@ class RotationActivity : AppCompatActivity() {
         if (angle == null) {
             Toast.makeText(this, "Введите корректный угол", Toast.LENGTH_SHORT).show()
         } else {
-            var bitmap = image as Bitmap
+            var bitmap: Bitmap = (rotationImage.drawable as BitmapDrawable).bitmap
             try {
                 bitmap = rotate.rotateImage(bitmap, angle)
-            } catch(error: Exception) {
-                Log.d("RotationActivity", "Произошла ошибка при работе алгоритма")
-                Toast.makeText(this, "Произошла ошибка при работе алгоритма", Toast.LENGTH_SHORT).show()
             }
-            rotationImage.setImageDrawable(null)
+            catch(error: Exception) {
+                Toast.makeText(this, "Ты лох", Toast.LENGTH_SHORT).show()
+            }
             rotationImage.setImageBitmap(bitmap)
-
-            val resultIntent = Intent()
-            try {
-                resultIntent.putExtra(RESULT_TAG, compressBitmap(bitmap))
-                setResult(RESULT_OK, resultIntent)
-            } catch (error: Exception) {
-                Log.d("RotationActivity", "Произошла ошибка при сжатии изображения")
-                Toast.makeText(this, "Произошла ошибка при сжатии изображения", Toast.LENGTH_SHORT).show()
-            }
         }
+    }
+
+    override fun finish() {
+        toast("I'm closed")
+        super.finish()
     }
 }
