18fa6505f74a3ec3b6e670ed768fb46ba1a2b867
Chantreck <minetvtc@yandex.ru>
2021-05-19 14:20:50 +0700

    Удалил неиспользуемые импорты

--- app/src/main/java/com/example/photoeditor/fragments/EffectsFragment.kt
+++ app/src/main/java/com/example/photoeditor/fragments/EffectsFragment.kt
@@ -1,13 +1,10 @@
 package com.example.photoeditor.fragments
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Parcelable
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.photoeditor.R
