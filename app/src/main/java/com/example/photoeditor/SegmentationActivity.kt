a02a3b55a489b6dd1bec04fcb53d6e9dd0702f85
Chantreck <minetvtc@yandex.ru>
2021-05-21 15:57:53 +0700

    Оптимизация кода
    Убрал лишние создания интентов, обновил айди вьюшек, в соответствии с разметкой, поменял getCapturedImage(uri) на стандартную функцию

--- app/src/main/java/com/example/photoeditor/SegmentationActivity.kt
+++ app/src/main/java/com/example/photoeditor/SegmentationActivity.kt
@@ -1,57 +1,56 @@
 package com.example.photoeditor
 
 import android.content.Intent
-import android.graphics.*
-import android.media.ExifInterface
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_segmentation.*
 import org.bytedeco.javacv.AndroidFrameConverter
 import org.bytedeco.javacv.OpenCVFrameConverter
 
 class SegmentationActivity : AppCompatActivity() {
-
-
     private val KEY = "Image"
-    private lateinit var uri: Uri
     private val RESULT_TAG = "resultImage"
     private lateinit var image: Bitmap
 
-    //TODO(Исправить получение изображения в сегментации)
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segmentation)
 
         FaceDetection.loadModel(this)
+        val resultIntent = Intent()
+
         val receivedImage = intent.getParcelableExtra<Parcelable>(KEY)
-        uri = receivedImage as Uri
         if (receivedImage != null) {
-
-            image = getCapturedImage(uri)
-            segmentationImage.setImageBitmap(image)
-
+            segmentationImage.setImageURI(receivedImage as Uri)
+            image = (segmentationImage.drawable as BitmapDrawable).bitmap
+//            image = getCapturedImage(receivedImage as Uri)
         }
+
         segmentationToolbar.setNavigationOnClickListener {
             this.finish()
         }
+
         applySegmentationButton.setOnClickListener() {
             image = detectFace(image)
+            segmentationImage.setImageBitmap(image)
         }
-        SaveSegmentationButton.setOnClickListener() {
-            val resultIntent = Intent()
+
+        saveButton.setOnClickListener() {
             try {
                 resultIntent.putExtra(RESULT_TAG, saveTempImage(this, image))
                 setResult(RESULT_OK, resultIntent)
-                Toast.makeText(this, "Изображение сохранено", Toast.LENGTH_LONG).show()
-                this.finish()
             } catch (error: Exception) {
                 Log.d("SegmentationActivity", "Произошла ошибка при сохранении изображения")
                 Toast.makeText(
@@ -63,7 +62,7 @@ class SegmentationActivity : AppCompatActivity() {
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N)
+    /*@RequiresApi(Build.VERSION_CODES.N)
     private fun getCapturedImage(uri: Uri): Bitmap {
         val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
         return when (ExifInterface(contentResolver.openInputStream(uri)).getAttributeInt(
@@ -99,7 +98,7 @@ class SegmentationActivity : AppCompatActivity() {
             )
             else -> bitmap
         }
-    }
+    }*/
 
     private fun detectFace(image: Bitmap): Bitmap {
         val strokeWidth = 15f
@@ -111,27 +110,22 @@ class SegmentationActivity : AppCompatActivity() {
         paint.setStyle(Paint.Style.STROKE)
         paint.setStrokeWidth(strokeWidth)
 
-
         val numberOfFaces = FaceDetection.detectFaces(mat)
 
         (0 until numberOfFaces.size()).forEach { i ->
             val rect = numberOfFaces[i]
             canvas.drawRect(
-                Rect(
-                    rect.x(),
-                    rect.y(),
-                    rect.x() + rect.width(),
-                    rect.y() + rect.height()
-                ), paint
+                Rect(rect.x(), rect.y(), rect.x() + rect.width(), rect.y() + rect.height()),
+                paint
             )
         }
+
         Toast.makeText(
             this,
             "Лиц распознано: ${numberOfFaces.size().toString()}",
             Toast.LENGTH_LONG
         ).show()
 
-        segmentationImage.setImageBitmap(mutableBitmap)
         return mutableBitmap
     }
 }
